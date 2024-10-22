22c741f83a83d9d2e1286e30b2e5010a9f7b6185
JuanCarlosIsordiaB <juancarlosisordiab@gmail.com>
2024-10-21 21:43:03 -0600

    OpenAI API endpoint

--- src/gpt/use-cases/spelling.use-case.ts
+++ src/gpt/use-cases/spelling.use-case.ts
@@ -2,8 +2,6 @@ import OpenAI from 'openai';
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 const genAI = new GoogleGenerativeAI(process.env.OPENAI_API_KEY);
 
-
-
 interface Options {
   prompt: string;
 }
@@ -14,9 +12,46 @@ export const spellingCheckerUseCase = async (
 ) => {
   const { prompt } = options;
 
-  const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
-  const result = await model.generateContent(['Dime como haz estado el dia de']);
-  
 
-  return result.response.text();
+  const completion = await openai.chat.completions.create({
+    messages: [
+      { 
+        role: "system", 
+        content: `
+        Te serán proveídos textos en español con posibles errores ortográficos y gramaticales,
+        Las palabras usadas deben de existir en el diccionario de la Real Academia Española,
+        Debes de responder en formato JSON, 
+        tu tarea es corregirlos y retornar información soluciones, 
+        también debes de dar un porcentaje de acierto por el usuario,
+        
+
+        Si no hay errores, debes de retornar un mensaje de felicitaciones.
+
+        Ejemplo de salida:
+        {
+          userScore: number,
+          errors: string[], // ['error -> solución']
+          message: string, //  Usa emojis y texto para felicitar al usuario
+        }
+        
+        
+        `
+      },
+      {
+        role: 'user',
+        content: prompt,
+      }
+  ],
+    model: "gpt-3.5-turbo-1106",
+    temperature: 0.3,
+    max_tokens: 150,
+    response_format: {
+      type: 'json_object'
+    }
+  });
+
+  // console.log(completion);
+  const jsonResp = JSON.parse(completion.choices[0].message.content);
+
+  return jsonResp;
 };
